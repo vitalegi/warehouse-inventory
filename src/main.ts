0d08932e30a3d863f22f60ac46d7d074835ece74
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2021-05-30 02:30:51 +0200

    removed vue pwa and updated caching policies

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
-import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
 import vuetify from "./plugins/vuetify";
 import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
 
 Vue.config.productionTip = false;
 
